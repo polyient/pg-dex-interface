c16e73b7af4dd99ed5fcaac0a83079e0e3743f1e
Scott Ferreira <scott.ferreira2@gmail.com>
2020-11-12 11:57:46 -0700

    additional tokenlist structure update

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,7 +1,6 @@
-//pg games default token list lives here
-
+//pg default token list lives here
 export const DEFAULT_TOKEN_LIST_URL = 'https://www.polyient.games/hosted/tokens.json?a=1'
-​
+
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
 ]
