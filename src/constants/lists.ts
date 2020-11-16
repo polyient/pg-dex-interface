a09dd0cfba79e68de61fe917784138cbb2046d2f
Scott Ferreira <scott.ferreira2@gmail.com>
2020-11-16 16:54:47 -0700

    updated token list for forced refresh

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,5 @@
 //pg default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://www.polyient.games/hosted/tokens.json?a=42'
+export const DEFAULT_TOKEN_LIST_URL = 'https://www.polyient.games/hosted/tokens.json?a=43'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
