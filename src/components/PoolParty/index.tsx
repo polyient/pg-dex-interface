2581223082df0e79c4012249265c83165bf501a3
Scott Ferreira <scott.ferreira2@gmail.com>
2020-11-16 18:46:11 -0700

    pool party text color update

--- src/components/PoolParty/index.tsx
+++ src/components/PoolParty/index.tsx
@@ -9,7 +9,7 @@ export function PoolParty() {
        <div className="dex-card p-4 mb-3">
         <h1 className="text-center pt-1">Pool Party I - Convert UNI Into PGT</h1>
         <h1 className="text-center pt-1">
-            Track Reward Growth on <a className="text-white" href="https://twitter.com/PolyientPool" target="_blank">Twitter</a>
+            <a className="text-decoration-none text-dex-success" href="https://twitter.com/PolyientPool" target="_blank">Track Reward Growth on Twitter</a>
         </h1>
         </div>
         </div>
