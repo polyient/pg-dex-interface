7b0879a7823e737f8f95beafff9d1c18a79968d9
Scott Ferreira <scott.ferreira2@gmail.com>
2020-11-23 16:36:55 -0700

    pool party announcement update 11/23/2020

--- src/components/PoolParty/index.tsx
+++ src/components/PoolParty/index.tsx
@@ -7,10 +7,7 @@ export function PoolParty() {
       <div className="row justify-content-center align-items-center h-100">
         <div className="col-md-5 mx-auto">
        <div className="dex-card p-4 mb-3">
-        <h1 className="text-center pt-1">Pool Party I - Convert UNI Into PGT</h1>
-        <h1 className="text-center pt-1">
-            <a className="text-decoration-none text-dex-success" href="https://twitter.com/PolyientPool" target="_blank">Track Reward Growth on Twitter</a>
-        </h1>
+        <h1 className="text-center pt-1">The UNI-PGT Pool Party I has concluded. We will be announcing the winners in the coming week.</h1>
         </div>
         </div>
         </div>
