e72cd5e2d9a9be6f0ebd0af6d1886606952f9415
Scott Ferreira <scott.ferreira2@gmail.com>
2020-12-29 10:09:41 -0700

    gala-pgu token list force update

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,5 @@
 //pg default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://app.polyient.games/hosted/tokens.json?a=16'
+export const DEFAULT_TOKEN_LIST_URL = 'https://app.polyient.games/hosted/tokens.json?a=18'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
