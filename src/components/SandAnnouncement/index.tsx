8cb064bb1eb91070eae2fe99e9eaf53f49aaa144
Scott Ferreira <scott.ferreira2@gmail.com>
2021-01-07 17:31:01 -0700

    forgot to make the text green as craig requested

--- src/components/SandAnnouncement/index.tsx
+++ src/components/SandAnnouncement/index.tsx
@@ -7,7 +7,7 @@ export function SandAnnouncement() {
       <div className="row justify-content-center align-items-center h-100">
         <div className="col-md-5 mx-auto">
        <div className="dex-card p-4 mb-3">
-        <h1 className="text-center pt-1">SAND-PGU trading is now live. Provide liquidity to earn PGU rewards. Click <a href="https://medium.com/polyient-games/polyient-games-and-the-sandbox-to-launch-sand-pgu-market-on-polyient-dex-117b7b62b6c0" target="_blank" className="text-white">here</a> to learn more.</h1>
+        <h1 className="text-center pt-1">SAND-PGU trading is now live. Provide liquidity to earn PGU rewards. Click <a href="https://medium.com/polyient-games/polyient-games-and-the-sandbox-to-launch-sand-pgu-market-on-polyient-dex-117b7b62b6c0" target="_blank" className="text-dex-success">here</a> to learn more.</h1>
         </div>
         </div>
         </div>
