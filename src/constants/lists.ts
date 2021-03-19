1a64e03da20bb2307155fb2f464fa081fcf8d722
Scott Ferreira <scott.ferreira2@gmail.com>
2021-03-18 18:08:54 -0700

    mega/pgu dex addition

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,5 @@
 //pg default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://app.polyient.games/hosted/tokens.json?a=22'
+export const DEFAULT_TOKEN_LIST_URL = 'https://app.polyient.games/hosted/tokens.json?a=24'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
